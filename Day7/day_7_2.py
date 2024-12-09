b2cffcd03cbc791cc1e2de1c7e8fbef05aec847d
Tim Baldwin <timwb499@gmail.com>
2024-12-09 10:15:06 -0600

    Completed Day 7 of the Advent of Code 2024

--- Day7/day_7_2.py
+++ Day7/day_7_2.py
@@ -17,26 +17,40 @@ with open(file_path, "r") as file:
         for number in numbers:
             values[value].append(int(number))
 
+def to_ternary(num):
+    if num == 0:
+        return '0'
+    digits = []
+    while num:
+        digits.append(str(num % 3))
+        num //= 3
+    return ''.join(reversed(digits))
+
 sum_of_values = 0
 largest_nums = 0
 
-for value in values:
+for v_num, value in enumerate(values):
     quantity = len(values[value])
-    combinations = pow(2, quantity) - 1
+    combinations = pow(3, quantity) - 1
+    
+    if v_num % 50 == 0:
+        print("Completed", v_num)
 
     current_num = 0
     while combinations > 0:
-        binary_num = bin(combinations)[2:]
-        binary_num = binary_num.zfill(quantity)
-
+        ternary_num = to_ternary(combinations)
+        ternary_num = ternary_num.zfill(quantity)
         for num_pos, num in enumerate(values[value]):
             if num_pos == 0:
                 current_num = num
             else:
-                if binary_num[num_pos] == "0":
+                if ternary_num[num_pos] == "0":
                     current_num = current_num * num
-                else:
+                elif ternary_num[num_pos] == "1":
                     current_num = current_num + num
+                else:
+                    temp_num = str(current_num) + "" + str(num)
+                    current_num = int(temp_num)
         
         if current_num == value:
             sum_of_values = sum_of_values + current_num
